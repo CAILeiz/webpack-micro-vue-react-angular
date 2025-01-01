7ef21bac533be0ec11a861c52acb2d4286fdff2b
CAILeiz <1912956608@qq.com>
2025-01-01 23:29:59 +0800

    feat: code split for entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,23 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
+    },
+    another: {
+      import: "./src/another-module.js",
+      dependOn: "shared",
+    },
+    shared: "lodash",
   },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-    }),
-  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/",
+    publicPath: "./",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
