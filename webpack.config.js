d7d51ab98fff133d1e48241651b039f096fb3a02
CAILeiz <1912956608@qq.com>
2025-01-02 00:02:49 +0800

    feat: import split to single bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,14 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    index: {
-      import: "./src/index.js",
-      dependOn: "shared",
-    },
-    another: {
-      import: "./src/another-module.js",
-      dependOn: "shared",
-    },
-    shared: "lodash",
+    index: "./src/index.js",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
     publicPath: "./",
-  },
-  optimization: {
-    runtimeChunk: "single",
   },
 };
